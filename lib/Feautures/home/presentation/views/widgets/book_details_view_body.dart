8109020ab3be70e8186fd00556402f7cca8a7d8a
mecodeur <mehdi.el2023@icloud.com>
2023-05-22 17:45:56 +0100

    Add custom scroll view to book details

--- lib/Feautures/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Feautures/home/presentation/views/widgets/book_details_view_body.dart
@@ -14,62 +14,71 @@ class BookDetailsViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: Column(
-        children: [
-          CustomBookDetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .2),
-            child: CustomBookImage(),
-          ),
-          SizedBox(
-            height: 43,
-          ),
-          Text(
-            'The Jungle Book',
-            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 6,
-          ),
-          Opacity(
-            opacity: .7,
-            child: Text(
-              'Rudyard Kipling',
-              style: Styles.textStyle18.copyWith(
-                  fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            child: Column(
+              children: [
+                CustomBookDetailsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * .2),
+                  child: CustomBookImage(),
+                ),
+                SizedBox(
+                  height: 43,
+                ),
+                Text(
+                  'The Jungle Book',
+                  style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Opacity(
+                  opacity: .7,
+                  child: Text(
+                    'Rudyard Kipling',
+                    style: Styles.textStyle18.copyWith(
+                        fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                SizedBox(
+                  height: 18,
+                ),
+                BookRating(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                BooksAction(),
+                Expanded(
+                  child: SizedBox(
+                    height: 40,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('You can also like',
+                      style: Styles.textStyle14.copyWith(
+                        fontWeight: FontWeight.w600,
+                      )),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                SimilarBooksListView(),
+                SizedBox(
+                  height: 40,
+                ),
+              ],
             ),
           ),
-          SizedBox(
-            height: 18,
-          ),
-          BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          BooksAction(),
-          SizedBox(
-            height: 30,
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text('You can also like',
-                style: Styles.textStyle14.copyWith(
-                  fontWeight: FontWeight.w600,
-                )),
-          ),
-          SizedBox(
-            height: 6,
-          ),
-          SimilarBooksListView(),
-          SizedBox(
-            height: 20,
-          ),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
