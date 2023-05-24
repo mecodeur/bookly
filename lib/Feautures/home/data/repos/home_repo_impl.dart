020e3c4a6f6a84addd437c7cb346b3f0912ad35b
mecodeur <mehdi.el2023@icloud.com>
2023-05-24 17:50:45 +0100

    Implement function feetchNewsetBooks

--- lib/Feautures/home/data/repos/home_repo_impl.dart
+++ lib/Feautures/home/data/repos/home_repo_impl.dart
@@ -1,13 +1,29 @@
 import 'package:bookly/Feautures/home/data/models/book_model/book_model.dart';
 import 'package:bookly/Feautures/home/data/repos/home_repo.dart';
 import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImpl implements HomeRepo{
+class HomeRepoImpl implements HomeRepo {
+
+  ApiService apiService;
+  HomeRepoImpl(this.apiService);
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> feetchNewsetBooks() async{
+
+    try {
+      var data = await apiService.get(endPoint: 'volumes?Filtering=free-ebooks&Sorting=newest &q=computer science');
+
+      List<BookModel> book = [];
+      for(var item in data['items']){
+            book.add(BookModel.fromJson(item));
+          }
+
+      return right(book);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
