4ed6170cdcb054e83fd67ee6e4be56549fdeb217
mecodeur <mehdi.el2023@icloud.com>
2023-05-30 17:05:32 +0100

    Implement feetch similar books (for book details)

--- lib/Feautures/home/data/repos/home_repo_impl.dart
+++ lib/Feautures/home/data/repos/home_repo_impl.dart
@@ -15,7 +15,7 @@ class HomeRepoImpl implements HomeRepo {
     try {
       var data = await apiService.get(
           endPoint:
-              'volumes?Filtering=free-ebooks&Sorting=relevance&q=subject:computer sience');
+          'volumes?Filtering=free-ebooks&Sorting=newest &q=computer science');
 
       List<BookModel> book = [];
       for (var item in data['items']) {
@@ -36,7 +36,28 @@ class HomeRepoImpl implements HomeRepo {
     try {
       var data = await apiService.get(
           endPoint:
-              'volumes?Filtering=free-ebooks&Sorting=relevance &q=subject:Programming');
+          'volumes?Filtering=free-ebooks&q=subject:Programming');
+
+      List<BookModel> book = [];
+      for (var item in data['items']) {
+        book.add(BookModel.fromJson(item));
+      }
+
+      return right(book);
+    } catch (e) {
+      if (e is DioError) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks({required String category}) async{
+    try {
+      var data = await apiService.get(
+          endPoint:
+          'volumes?Filtering=free-ebooks&Sorting=relevance &q=subject:Programming');
 
       List<BookModel> book = [];
       for (var item in data['items']) {
